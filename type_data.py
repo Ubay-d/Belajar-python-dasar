2ce1b443df37e5a85a3e3c4ab1e4e9e848caf871
LINI2019 <ubay.masykur@gmail.com>
2025-04-29 22:13:17 +0700

    type_data.py

---
+++ type_data.py
@@ -0,0 +1,48 @@
+print("=========== Belajar Tipe Data ===================")
+print("Tipe Data Dasar")
+print("")
+
+print("string : Teks")
+nama = "Ubaydillah"
+alamat = "Jakarta"
+print("Nama saya:", nama)
+print("Alamat saya:", alamat)
+print('Type dari nama:', type(nama))
+print()
+
+print("int / integer : Bilangan Bulat")
+umur = 20
+tinggi = 170
+berat = 52
+print("Umur saya:", umur)
+print("Tinggi saya:", tinggi)
+print("Berat saya:", berat)
+print('Type dari berat:', type(berat))
+print()
+
+print("float : Bilangan Desimal")
+lingkar_perut = 80.5
+print("Lingkar perut saya:", lingkar_perut)
+print('Type dari lingkar_perut:', type(lingkar_perut))
+print()
+
+print("boolean : True / False")
+tidur = True
+makan = False
+print("Apakah saya tidur?", tidur)
+print("Apakah saya makan?", makan)
+print('Type dari tidur:', type(tidur))
+print('Type dari makan:', type(makan))
+print()
+
+print("list : Kumpulan Data")
+print("Nama saya:", nama)
+print("Umur saya:", umur)
+print("Tinggi saya:", tinggi)
+print("Berat saya:", berat)
+print("Lingkar perut saya:", lingkar_perut)
+print("Tidur:", tidur)
+print("Makan:", makan)
+print("Data saya:", [nama, umur, tinggi, berat, lingkar_perut, tidur, makan])
+print('Type dari data saya:', type([nama, umur, tinggi, berat, lingkar_perut, tidur, makan]))
+print()
